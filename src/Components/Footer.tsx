38e735cb8199fd84c253c4f4f1e46f452d02ff75
JaxProjex <strattonja@protonmail.com>
2024-12-19 20:55:41 -0800

    added quick link footer edits

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -6,20 +6,17 @@ function Header(props:any) {
         <div className={"footer"}>
             <div className={"footer-row"}>
             <div className={"footer-content-container"} onClick={() => {window.open("https://google.com")}}>
-                <span className={"footer-content-icon"}>IMG1</span><span className={"footer-content-text"}> = LinkedIn</span>
+                <span className={"footer-content-icon"}>[in]</span><span className={"footer-content-text"}> = LinkedIn</span>
             </div>
 
             <div className={"footer-content-container"} onClick={() => {window.open("https://google.com")}}>
-                <span className={"footer-content-icon"}>IMG2</span><span className={"footer-content-text"}> = GitHub</span>
+                <span className={"footer-content-icon"}>[gh]</span><span className={"footer-content-text"}> = GitHub</span>
             </div>
 
             <div className={"footer-content-container"} onClick={() => {window.open("https://google.com")}}>
-                <span className={"footer-content-icon"}>IMG3</span><span className={"footer-content-text"}> = Portfolio</span>
+                <span className={"footer-content-icon"}>[@]</span><span className={"footer-content-text"}> = Resume</span>
             </div>
 
-            <div className={"footer-content-container"} onClick={() => {window.open("https://google.com")}}>
-                <span className={"footer-content-icon"}>IMG4</span><span className={"footer-content-text"}> = Resume</span>
-            </div>
             </div>
         </div>
     )
